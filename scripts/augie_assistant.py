ed4b88f602e4f99d5a551adce8a8bbd9ba86a31e
shashwatmaharjan <shashwatmjn@gmail.com>
2024-10-06 22:19:16 -0400

    Added base code framework for the assistant

---
+++ scripts/augie_assistant.py
@@ -0,0 +1,15 @@
+# Import necessary libraries
+import os
+
+# Main function
+def main():
+
+    pass
+
+if __name__ == "__main__":
+
+    # Clear the screen based on the operating system
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Run the main function
+    main()
